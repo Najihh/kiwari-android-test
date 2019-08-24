60d9dbb0ce1052fbf5c6af6534e2282b5d87155f
makesmeinspire <muhammad.iqbal.najih@gmail.com>
2019-08-24 14:18:13 +0700

    add layout

---
+++ app/src/main/java/com/senjacreative/kiwarichat/Chatroom.java
@@ -0,0 +1,14 @@
+package com.senjacreative.kiwarichat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chatroom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatroom);
+    }
+}
